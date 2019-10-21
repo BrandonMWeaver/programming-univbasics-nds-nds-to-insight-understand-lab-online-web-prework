e7059586ea20b1f0e06e94d6990d57acabd71957
Brandon Weaver <a7xncob@gmail.com>
2019-10-21 23:48:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,12 @@ def print_first_directors_movie_titles
   i = 0
   while i < directors_database.size do
     puts directors_database[i]
+    if directors_database[i][:name] == "Stephen Spielberg"
+      j = 0
+      while j < directors_database[i][:movies] do
+        puts "#{directors_database[i][:movies][j]}\n"
+        j += 1
+      end
     i += 1
   end
   
